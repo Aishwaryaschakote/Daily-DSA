278bff2e399e46685e190d16b9561c0f990c95ac
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-12 21:38:11 +0530

    day 4

---
+++ Basics/arrWithPassByRef.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void doSomething(int arr[], int n){
+    arr[0] += 100;
+    cout<<arr[0]<< " this is the value in doSome...function"<<endl;
+}
+
+int main(){
+    int n=5;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    doSomething(arr,n);
+    cout<<arr[0]<<" this is the value in main fun";
+    return 0;
+}
