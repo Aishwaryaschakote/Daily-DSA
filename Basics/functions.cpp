99c60d9c2085cef80d601bf1058c7b0fd8aaa64d
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-12 21:24:28 +0530

    day 4

---
+++ Basics/functions.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int modify(int a){
+    int b= a+10;
+    return b; 
+}
+
+int main(){
+    int a=10;
+    cout<< modify(a) <<" is the total value";
+    return 0;
+}
---
+++ Basics/passbyval.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void string_fun(string s){
+    s+="web";
+    cout<<s<<endl;
+}
+
+int main(){
+    string s= "aish";
+    string_fun(s);
+    cout << s<<endl;
+    return 0;
+}
