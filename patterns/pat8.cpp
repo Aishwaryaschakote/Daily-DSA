28cd94cba8902b491a4c22c83678a999e0e9a3af
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-18 01:32:28 +0530

    dsa 6

---
+++ patterns/pat8.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        //spaces
+        for(int j=1; j<=i-1; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int k=1; k<=2*n-(2*i-1); k++){
+            cout<<"*";
+        }
+        //spaces
+        for(int m=1; m<=i-1; m++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
