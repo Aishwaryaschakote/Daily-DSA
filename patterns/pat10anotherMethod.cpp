b6a62c7c3234e8376a1e7fd63bf5034fe9ebaaec
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-24 23:22:57 +0530

    day 7

---
+++ patterns/pat10anotherMethod.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter n: "<<endl;
+    cin>>n;
+
+    for(int i=1; i<=2*n-1; i++){
+        for(int j=1; j<=2*n-i; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    
+return 0;
+}
