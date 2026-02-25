567a393e1dec9ffd62e3171d7f5e1c38a3c2d0b7
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-25 19:46:29 +0530

    day 7

--- patterns/pat11partb.cpp
+++ patterns/pat11partb.cpp
@@ -17,6 +17,19 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+    for(int i=0; i<n; i++){
+        int start=0;
+        if(i%2==0) start = 0;
+        else start =1;
+        for(int j=1; j<=n-i;j++){
+           
+            //flipping the 0 to 1 and 1 to 0
+            cout<<((start ==1) ? " J " : " 0 ");
+            start= 1-start;
+        }
+        cout<<endl;
+    }
     
     
 return 0;
