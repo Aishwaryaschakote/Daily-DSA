9c43ae8d2e3cfde01c1f4e34f36e3671effaa7a4
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-25 19:02:33 +0530

    day 8

--- patterns/pat10anotherMethod.cpp
+++ patterns/pat10anotherMethod.cpp
@@ -6,12 +6,20 @@ int main(){
     cout<<"enter n: "<<endl;
     cin>>n;
 
-    for(int i=1; i<=2*n-1; i++){
-        for(int j=1; j<=2*n-i; j++){
-            cout<<" * ";
+    for(int i=1; i<=n; i++){
+        int start=1;
+        for(int j=1; j<=i; j++){
+            if(i%2 == 1){
+                cout<<" "<<start;
+            }
+            else{
+                start = 0;
+                cout<<" "<<start;
+            }
         }
         cout<<endl;
     }
+    
     
 return 0;
 }
