ebf4dafbb8f245a2aab72137670f9d1b11560c64
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-02-25 19:14:01 +0530

    day 7

--- patterns/pat11.cpp
+++ patterns/pat11.cpp
@@ -9,11 +9,12 @@ int main(){
     for(int i=1; i<=n; i++){
         int start=1;
         for(int j=1; j<=i; j++){
-            if(i%2 == 1){
+            if(i%2 == 1){ 
+                
                 cout<<" "<<start;
             }
             else{
-                start = 0;
+                start = 1-start;
                 cout<<" "<<start;
             }
         }
