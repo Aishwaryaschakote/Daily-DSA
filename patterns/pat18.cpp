d8a1dc78c713b0e196b540446fea811609176179
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-04-03 01:30:10 +0530

    day 13

--- patterns/pat18.cpp
+++ patterns/pat18.cpp
@@ -5,9 +5,21 @@ int main(){
     int n;
     cout<<"Enter n:"<<endl;
     cin>>n;
-    for(int i= 0; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout<<" "<<char(i+64)<<" ";
+    for(int i= 0; i<n; i++){
+        //spaces
+
+        for(int j=1; j<=n-i-1; j++){
+            cout<<" ";
+        }
+        char ch='A';
+        int breakpoint = (2*i+1)/2;
+        for(int j=1; j<=2*i+1; j++){
+            cout<<ch;
+        }
+        //spaces
+
+        for(int j=1; j<=n-i-1; j++){
+            cout<<" ";
         }
        
         cout<<endl;
