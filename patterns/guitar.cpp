692b04ff304bdb0c7149a0767ca6385861f50548
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-04-04 02:44:58 +0530

    day 14

---
+++ patterns/guitar.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+// Lower body
+    cout << "        ****" << endl;
+    cout << "        ****" << endl;
+    // Neck
+    for(int i = 0; i < 4; i++) {
+        cout << "         **" << endl;
+    }
+
+    // Lower body
+    cout << "        *****" << endl;
+    cout << "       *******" << endl;
+    cout << "      ***    ***" << endl;
+    cout << "     ***      ***" << endl;
+    cout << "     ****    ****" << endl;
+    cout << "      *********" << endl;
+    cout << "        *****" << endl;
+
+    return 0;
+}
