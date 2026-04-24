501a01ceb276ae9dea1d3b381e055eb8403bd073
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-04-24 16:07:26 +0530

    day 19

---
+++ sorting techniques/insertionSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<int>inserTionSort(vector<int>& arr, int n){
+
+    for(int i = 0; i<= n-1; i++){
+        int j=i;
+        while (j>0 && arr[j-1]>arr[j])
+        {
+           int temp = arr[j];
+           arr[j] = arr[j-1];
+           arr[j-1] = temp;
+           j--;
+        }
+        
+    }
+
+    return arr;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i = 0; i< n; i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+
+    inserTionSort(arr, n);
+
+    for(auto a: arr)cout<<a<<" ";
+    return 0;
+}
