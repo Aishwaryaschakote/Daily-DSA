65772043792298b155129d14c5857cfca8648bb8
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-04-24 15:18:51 +0530

    day 19

---
+++ sorting techniques/bubbleSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<int>bubbleSort(vector<int>&arr, int n){
+
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 0; j <= i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+    }
+    }
+    return arr;
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+
+    for(int i=0; i< n; i++){
+        cin>>arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    for(auto s: arr){
+        cout<<s<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
