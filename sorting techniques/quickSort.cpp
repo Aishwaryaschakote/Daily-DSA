4d8093d220ed878b491a154b20ec1526ec36551a
Aishwarya Chakote <112263404+Aishwaryaschakote@users.noreply.github.com>
2026-04-26 16:48:32 +0530

    day 20

--- sorting techniques/quickSort.cpp
+++ sorting techniques/quickSort.cpp
@@ -2,30 +2,30 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-
+// function to find a correct place for pivot....return j
 int partition(vector<int>&arr, int low, int high){
 
     int pivot = arr[low];
     int i = low;
     int j = high;
 
-    while (i<j)
+    while (i<j)//runs until i crosses j or vicevarsa
     {
-        while (i<=high  && arr[i]<=pivot)
+        while (i<=high  && arr[i]<=pivot)// checks whether left side ele are lesser or greater than pivot if greater stops if lesser continues
         {
             i++;
         }
         
-        while (j>=low  && arr[j]>pivot)
+        while (j>=low  && arr[j]>pivot)// same as above
         {
             j--;
         }
 
-        if(i<j)swap(arr[i], arr[j]);
+        if(i<j)swap(arr[i], arr[j]); //swaps else if i and j are not crossed yet
     }
-    swap(arr[j], arr[low]);
+    swap(arr[j], arr[low]);//when the while loop breaks then pivot ele is swaped with jth pos ele..
     
-    return j;
+    return j;//returns the pos where the pivot has to be placed
 }
 
 vector<int> quickSort(vector<int>&arr, int low, int high){
